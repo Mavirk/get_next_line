ba5c3d269fc7b582a82be1570d76e87d84faad1a
Mavirk <oatsrobert81@gmail.com>
2017-11-24 00:53:20 +0200

    worked fromm home commiot 5

--- get_next_line.c
+++ get_next_line.c
@@ -6,59 +6,35 @@ int get_next_line(const int fd, char **line)
 	char			*tempadd;
 	char			*ret;
 	char			*test;
-	char			*leftp;
-	char			*carryp;
 	int 			readsize;
 
-if (!save)
-{
-	save = malloc(BUFF_SIZE);
-	readsize = read(fd, save, BUFF_SIZE);
-	if ((leftp = ft_strchr(save, '\n')))
-	{ 
-		*(save + BUFF_SIZE + 1) = '\0'; 
-		*leftp = '\0'; 
-		*line = strdup(save);
-		leftp++;
-		carryp = strdup(leftp); 
-		free(save);
-		save = strcpy(save, carryp);
-	}
-	else 
-	{	
+	if (!save)
+	{
+		save = malloc(BUFF_SIZE);
+		readsize = read(fd, save, BUFF_SIZE);
 		while (readsize > 0 && !(ft_strchr(save, '\n')))
-		{
-			tempadd = ft_strdup(save);
-		//	ft_putstr("save ");			
-		//	ft_putendl(save);
-		//	ft_putstr("tempadd ");
-		//	ft_putendl(tempadd);
-			free(save);
-			save = (char*)malloc(BUFF_SIZE + strlen(tempadd) + 1);
-			save = ft_strcpy(save,tempadd);
-			test = (save + ft_strlen(tempadd));
-			readsize = read(fd, test, BUFF_SIZE);	
-		//	ft_putendl(tempadd);
-			//ft_putendl(" : tempadd after read");
-			ret = ft_strdup(tempadd);
-			ret[ft_strlen(tempadd)] = '\0';
-		//	ft_putstr("save ");			
-		//	ft_putendl(save);
-		//	ft_putstr("tempadd ");
-		//	ft_putendl(tempadd);
-		//	ft_putstr("ret ");
-		//	ft_putendl(ret);
-			free (tempadd);	
-		} 
+			{
+				tempadd = ft_strdup(save);
+				free(save);
+				save = (char*)malloc(BUFF_SIZE + strlen(tempadd) + 1);
+				save = ft_strcpy(save,tempadd);
+				test = (save + ft_strlen(tempadd));
+				readsize = read(fd, test, BUFF_SIZE);		
+				ret = ft_strdup(tempadd);
+				ret[ft_strlen(tempadd)] = '\0';
+				free (tempadd);	
+			} 
+		*ft_strchr(save, '\n') = '\0';
+		ret = save;
+		*line = ft_strdup(ret);
+		save = (ret + (strlen(ret)));
+		ft_putstr(save);
+		free(ret);
+		return (1);
 	}
-	*ft_strchr(save, '\n') = '\0';
-	ret = save;
-	*line = ft_strdup(ret);
-return(1);
+	return (1);
 }
-return(1);
-}	
-
+	
 #include <fcntl.h>
 
 int		main(int argc, char **argv)
--- get_next_line.h
+++ get_next_line.h
@@ -4,7 +4,7 @@
 #include <stdlib.h>
 #include "Libft/libft.h"
 
-# define BUFF_SIZE 40 
+# define BUFF_SIZE 7 
 
 int		get_next_line(const int fd, char **line);
 
