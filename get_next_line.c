0f3a5a2ac9014c49cd465314503f9398cf75dd92
Mavirk <oatsrobert81@gmail.com>
2017-11-25 09:02:57 +0200

    go google static variables

--- get_next_line.c
+++ get_next_line.c
@@ -4,54 +4,43 @@ int get_next_line(const int fd, char **line)
 {
 	static char 	*save = NULL;
 	char			*tempadd;
-	char			*ret;
-	char			*test;
 	int 			readsize;
 
 	if (!save)
 	{
-		ft_putstr("first iteration");
 		save = malloc(BUFF_SIZE);
 		readsize = read(fd, save, BUFF_SIZE);
 		while (readsize > 0 && !(ft_strchr(save, '\n')))
-			{
-				tempadd = ft_strdup(save);
-				free(save);
-				save = (char*)malloc(BUFF_SIZE + strlen(tempadd) + 1);
-				save = ft_strcpy(save,tempadd);
-				test = (save + ft_strlen(tempadd));
-				readsize = read(fd, test, BUFF_SIZE);		
-				ret = ft_strdup(tempadd);
-				ret[ft_strlen(tempadd)] = '\0';
-				free (tempadd);	
-			} 
-		*ft_strchr(save, '\n') = '\0';
-		tempadd = ft_strchr(save, '\0');
-		tempadd++;
-		ret = save;
-		*line = ft_strdup(ret);
-		free (save);
-		save = malloc(BUFF_SIZE - ft_strlen(ret));
-		save = (tempadd);
-		ft_putnbr(ft_strlen(save));
-		ft_putstr(save);
-		ft_putstr(tempadd);
-		ft_putstr("2");
-		free(ret);
-		return (1);
+		{
+			tempadd = ft_strdup(save);
+			free(save);
+			save = (char*)malloc(BUFF_SIZE + ft_strlen(tempadd) + 1);
+			save = ft_strcpy(save,tempadd);
+			readsize = read(fd, (save + ft_strlen(tempadd)), BUFF_SIZE);
+			free (tempadd);	
+		} 
+		ft_putstr("first iteration");
 	}
 	else
 	{
-		ft_putstr("second iteration");
-		ft_putnbr(ft_strlen(save));
 		readsize = 1;
-		if (readsize > 0 && !(ft_strchr(save, '\n')))
-			{
-				ft_putstr(save);
-			} 
-		*line = ft_strdup("test");
-		return (1);
+		while (readsize > 0 && !(ft_strchr(save, '\n')))
+		{
+			tempadd = ft_strdup(save);
+			save = (char*)malloc(BUFF_SIZE + ft_strlen(tempadd) + 1);
+			save = ft_strcpy(save,tempadd);
+			ft_putendl(save);
+			readsize = read(fd, (save + ft_strlen(tempadd)), BUFF_SIZE);
+			ft_putnbr(readsize);
+			free (tempadd);	
+		} 
+		ft_putstr("second iteration");
 	}
+	*ft_strchr(save, '\n') = '\0';
+	*line = ft_strdup(save);
+	save = (ft_strchr(save, '\0') + 1);
+	if (readsize < 0)
+		return (0);
 	return (1);
 }
 	
@@ -68,14 +57,12 @@ int		main(int argc, char **argv)
 		fd = open(argv[1], O_RDONLY);
 	else
 		return (2);
-	if (get_next_line(fd, &line) == 1)
+	while (get_next_line(fd, &line) == 1)
+	{
 		ft_putstr(" output : ");
 		ft_putendl(line);
 		free(line);
-	if (get_next_line(fd, &line) == 1)
-		ft_putstr(" output : ");
-		ft_putendl(line);
-		//free(line);		
+	}	
 	if (argc == 2)
 		close(fd);
 	return (0);
