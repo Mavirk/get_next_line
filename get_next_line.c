f481774b8bc0159205d7bea2904672f0c95be128
Robert MURDOCH <rmurdoch@e4r6p2.wethinkcode.co.za>
2017-12-01 18:49:46 +0200

    normalized my code so that it is readable and i format the code throughout

--- get_next_line.c
+++ get_next_line.c
@@ -1,143 +1,86 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rmurdoch <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/12/01 18:40:32 by rmurdoch          #+#    #+#             */
+/*   Updated: 2017/12/01 18:40:41 by rmurdoch         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "get_next_line.h"
 
-char 	*read_line(char **save, int *rsize, char **temp, int fd)
+void	read_line(char **sv, int *rsize, char **t, int fd)
 {
-	while (!ft_strchr(*save,'\n') && *rsize > 0)
+	while (!ft_strchr(*sv, '\n') && *rsize > 0)
 	{
-
-		*temp = ft_strdup(*save);
-		*save = (char *)malloc(BUFF_SIZE + ft_strlen(*temp));
-		ft_strcpy(*save, *temp);
-		*rsize = read(fd, (*save + ft_strlen(*temp)), BUFF_SIZE);
-		*(*save + ft_strlen(*temp) + BUFF_SIZE) = '\0';
-		free(*temp);
-	}	
-	return("yes");
+		*t = ft_strdup(*sv);
+		*sv = (char *)malloc(BUFF_SIZE + ft_strlen(*t));
+		ft_strcpy(*sv, *t);
+		*rsize = read(fd, (*sv + ft_strlen(*t)), BUFF_SIZE);
+		*(*sv + ft_strlen(*t) + BUFF_SIZE) = '\0';
+		free(*t);
+	}
 }
 
-int	check_save(char **save, char **temp, char ***line)
+int		chk_sv(char **sv, char **t, char ***line)
 {
-	char *temp2;
+	char *t2;
 
-	if (ft_strchr(*save, '\n'))
+	if (ft_strchr(*sv, '\n'))
 	{
-		if (ft_strlen(*save) == 1)
+		if (ft_strlen(*sv) == 1)
 		{
-			ft_strdel(save);
-			return(0);
+			ft_strdel(sv);
+			return (0);
 		}
-		*temp = ft_strchr(*save, '\n') + 1;
-		*(ft_strchr(*save, '\n')) = '\0';
-		**line = ft_strdup(*save);
-		if (**temp != '\0')
+		*t = ft_strchr(*sv, '\n') + 1;
+		*(ft_strchr(*sv, '\n')) = '\0';
+		**line = ft_strdup(*sv);
+		if (**t != '\0')
 		{
-			temp2 = *save;
-			*save = ft_strdup(*temp);
-			ft_strdel(&temp2);
+			t2 = *sv;
+			*sv = ft_strdup(*t);
+			ft_strdel(&t2);
 		}
-		else 
-			ft_strdel(save);
+		else
+			ft_strdel(sv);
 		return (0);
 	}
 	return (1);
 }
 
-int get_next_line(const int fd, char **line)
+int		get_next_line(const int fd, char **line)
 {
-	static char *save = NULL;
-	char		*temp;
+	static char *sv = NULL;
+	char		*t;
 	int			rsize;
 
 	rsize = 1;
-	if (!save)
+	if (!sv)
 	{
-		save = (char *)malloc(BUFF_SIZE + 1);
-		rsize = read(fd, save, BUFF_SIZE);
-		save[rsize] = '\0';
-		if (rsize == 0 || (rsize == 1 && ft_strcmp(save, "\n")))
+		sv = (char *)malloc(BUFF_SIZE + 1);
+		rsize = read(fd, sv, BUFF_SIZE);
+		sv[rsize] = '\0';
+		if (rsize == 0 || (rsize == 1 && ft_strcmp(sv, "\n")))
 		{
-			ft_strdel(&save);
-			return(0);
+			ft_strdel(&sv);
+			return (0);
 		}
 	}
-	read_line(&save, &rsize, &temp, fd);
+	read_line(&sv, &rsize, &t, fd);
 	if (rsize < 0)
 	{
-		if (*save)
-			ft_strdel(&save);
-		return(-1);
+		if (*sv)
+			ft_strdel(&sv);
+		return (-1);
 	}
-	if (check_save(&save, &temp, &line) && *save && !(ft_strchr(save, '\n')) && rsize == 0)
+	if (chk_sv(&sv, &t, &line) && *sv && !(ft_strchr(sv, '\n')) && rsize == 0)
 	{
-		*line = ft_strdup(save);
-		ft_strdel(&save);
+		*line = ft_strdup(sv);
+		ft_strdel(&sv);
 	}
-	return(1);
+	return (1);
 }
-/*
-#include <string.h>
-#include <stdio.h>
-#include <fcntl.h>
-#include <unistd.h>
-
-int				main(void)
-{
-	char		*line;
-	int			fd;
-	int			ret;
-	int			count_lines;
-	char		*filename;
-	int			errors;
-
-	filename = "test.txt";
-	fd = open(filename, O_RDONLY);
-	if (fd > 2)
-	{
-		count_lines = 0;
-		errors = 0;
-		line = NULL;
-		while ((ret = get_next_line(fd, &line)) > 0)
-		{
-			if (count_lines == 0 && strcmp(line, "1234567") != 0)
-				errors++;
-			if (count_lines == 1 && strcmp(line, "abcdefgh") != 0)
-				errors++;
-			count_lines++;
-			if (count_lines > 50)
-				break ;
-		}
-		close(fd);
-		if (count_lines != 2)
-			printf("-> must have returned '1' twice instead of %d time(s)\n", count_lines);
-		if (errors > 0)
-			printf("-> must have read \"1234567\" and \"abcdefgh\"\n");
-		if (count_lines == 2 && errors == 0)
-			printf("OK\n");
-	}
-	else
-		printf("An error occured while opening file %s\n", filename);
-	return (0);
-}
-
-#include <fcntl.h>
-int		main(int argc, char **argv)
-{
-	int		fd;
-	char	*line;
-
-	if (argc == 1)
-		fd = 0;
-	else if (argc == 2)
-		fd = open(argv[1], O_RDONLY);
-	else
-		return (2);
-	while (get_next_line(fd, &line) == 1)
-	{
-		ft_putstr("output: ");
-		ft_putendl(line);
-		free(line);
-	}	
-	if (argc == 2)
-		close(fd);
-	return (0);
-}*/
--- test.c
+++ test.c
@@ -50,3 +50,71 @@ int main(void)
     ft_putendl("\nBREAK\n");
 }
 
+/*
+#include <string.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+int             main(void)
+{
+    char        *line;
+    int         fd;
+    int         ret;
+    int         count_lines;
+    char        *filename;
+    int         errors;
+
+    filename = "test.txt";
+    fd = open(filename, O_RDONLY);
+    if (fd > 2)
+    {
+        count_lines = 0;
+        errors = 0;
+        line = NULL;
+        while ((ret = get_next_line(fd, &line)) > 0)
+        {
+            if (count_lines == 0 && strcmp(line, "1234567") != 0)
+                errors++;
+            if (count_lines == 1 && strcmp(line, "abcdefgh") != 0)
+                errors++;
+            count_lines++;
+            if (count_lines > 50)
+                break ;
+        }
+        close(fd);
+        if (count_lines != 2)
+            printf("-> must have returned '1' twice instead of %d time(s)\n", count_lines);
+        if (errors > 0)
+            printf("-> must have read \"1234567\" and \"abcdefgh\"\n");
+        if (count_lines == 2 && errors == 0)
+            printf("OK\n");
+    }
+    else
+        printf("An error occured while opening file %s\n", filename);
+    return (0);
+}
+
+#include <fcntl.h>
+int     main(int argc, char **argv)
+{
+    int     fd;
+    char    *line;
+
+    if (argc == 1)
+        fd = 0;
+    else if (argc == 2)
+        fd = open(argv[1], O_RDONLY);
+    else
+        return (2);
+    while (get_next_line(fd, &line) == 1)
+    {
+        ft_putstr("output: ");
+        ft_putendl(line);
+        free(line);
+    }   
+    if (argc == 2)
+        close(fd);
+    return (0);
+}*/
+
