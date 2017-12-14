2d464ef882054b66385734efae331c16cce09130
Robert MURDOCH <rmurdoch@e4r6p2.wethinkcode.co.za>
2017-12-14 16:58:20 +0200

    added working dirs and libs

---
+++ get_next_line.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rmurdoch <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/12/01 18:55:35 by rmurdoch          #+#    #+#             */
+/*   Updated: 2017/12/13 11:17:44 by rmurdoch         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <unistd.h>
+# include <stdlib.h>
+# include "libft/includes/libft.h"
+# define BUFF_SIZE 4
+
+int		get_next_line(const int fd, char **line);
+
+#endif
